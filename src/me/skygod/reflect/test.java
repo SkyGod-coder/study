e8588d344ce70049ad3e0db747ce27ba847e9e90
BuildTools <unconfigured@null.spigotmc.org>
2021-08-09 19:37:38 +0800

    上传到github

--- src/me/skygod/reflect/test.java
+++
@@ -1,7 +0,0 @@
-package me.skygod.reflect;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println(test.class.getClassLoader().getResource("config.properties"));
-    }
-}
